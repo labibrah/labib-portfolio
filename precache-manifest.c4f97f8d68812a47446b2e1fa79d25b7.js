9813df0067da8246ac0db6a30c1854e062a2d236
Labib Rahman <labib.rahman@northsouth.edu>
2022-01-17 00:55:12 +0600

    Updates

---
+++ precache-manifest.c4f97f8d68812a47446b2e1fa79d25b7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629256eefc53090e0a2dfc1c69feadcd",
+    "url": "/labibrah/labib-portfolio/index.html"
+  },
+  {
+    "revision": "74b1ab1d33f95ffb4e2d",
+    "url": "/labibrah/labib-portfolio/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "48727b4fb4d5484ba57a",
+    "url": "/labibrah/labib-portfolio/static/css/main.bea8031f.chunk.css"
+  },
+  {
+    "revision": "74b1ab1d33f95ffb4e2d",
+    "url": "/labibrah/labib-portfolio/static/js/2.8dd0c50c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/labibrah/labib-portfolio/static/js/2.8dd0c50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48727b4fb4d5484ba57a",
+    "url": "/labibrah/labib-portfolio/static/js/main.56369db0.chunk.js"
+  },
+  {
+    "revision": "6ba5ce51471a145cb3fc",
+    "url": "/labibrah/labib-portfolio/static/js/runtime-main.fe553cf7.js"
+  },
+  {
+    "revision": "3d45f423d62a5020c05c928ad8772391",
+    "url": "/labibrah/labib-portfolio/static/media/arrow.3d45f423.svg"
+  },
+  {
+    "revision": "a74212b632dc205e2bd832acaa46a2e9",
+    "url": "/labibrah/labib-portfolio/static/media/arrow.a74212b6.svg"
+  },
+  {
+    "revision": "04e4f259c38bea403acb2148d9f90aa0",
+    "url": "/labibrah/labib-portfolio/static/media/contact-info-icon1.04e4f259.svg"
+  },
+  {
+    "revision": "df4fdedf2f261b6bd88551a4f1ab92f9",
+    "url": "/labibrah/labib-portfolio/static/media/contact-info-icon2.df4fdedf.svg"
+  },
+  {
+    "revision": "3cf93bcfa4d4d754bc4675b37bc0361b",
+    "url": "/labibrah/labib-portfolio/static/media/contact-info-icon3.3cf93bcf.svg"
+  },
+  {
+    "revision": "0f4894f4f8c35217468912ffb40ce54a",
+    "url": "/labibrah/labib-portfolio/static/media/contactBackground.0f4894f4.png"
+  },
+  {
+    "revision": "bb2da441dbfafcc70be7b09e8de384e2",
+    "url": "/labibrah/labib-portfolio/static/media/github.bb2da441.svg"
+  },
+  {
+    "revision": "1800e3d6ed5bee24d81f266139d74790",
+    "url": "/labibrah/labib-portfolio/static/media/hero-image.1800e3d6.png"
+  },
+  {
+    "revision": "94d9daa12d42e64c5b43fa5165541826",
+    "url": "/labibrah/labib-portfolio/static/media/linkedin.94d9daa1.svg"
+  },
+  {
+    "revision": "3825e32be64f9990c46b47aa91f76faf",
+    "url": "/labibrah/labib-portfolio/static/media/logo-yellow.3825e32b.svg"
+  },
+  {
+    "revision": "0e7ffccc0c2f339f93ea8bb87c331c9c",
+    "url": "/labibrah/labib-portfolio/static/media/logo.0e7ffccc.svg"
+  },
+  {
+    "revision": "4efe88e477faec8f83eaf997884b3da5",
+    "url": "/labibrah/labib-portfolio/static/media/logo.4efe88e4.svg"
+  },
+  {
+    "revision": "f521cf4553d6d240c47e135be9ba7a9c",
+    "url": "/labibrah/labib-portfolio/static/media/mobile-close.f521cf45.svg"
+  },
+  {
+    "revision": "10823520de33dffb05e6963a0d8c7bec",
+    "url": "/labibrah/labib-portfolio/static/media/mobile-menu.10823520.svg"
+  },
+  {
+    "revision": "c8ee3923bdfab3e3082d32ee4331cbbd",
+    "url": "/labibrah/labib-portfolio/static/media/preview.c8ee3923.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/labibrah/labib-portfolio/precache-manifest.c4f97f8d68812a47446b2e1fa79d25b7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/labibrah/labib-portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
